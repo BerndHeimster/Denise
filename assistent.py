1133bdbff69c4428224a5d0253a20ba66e950e96
Your Name <moellerjuljano@gmail.com>
2022-01-06 19:59:01 +0100

    init

--- assistent.py
+++ assistent.py
@@ -2,6 +2,7 @@ import mappings as mappings
 from neuralintents import GenericAssistant
 import pyttsx3
 import nltk
+from google import google
 import wikipediaapi
 
 from playsound import playsound
@@ -11,18 +12,17 @@ from playsound import playsound
 nltk.download('omw-1.4')
 
 
-
 #for the output
 speaker = pyttsx3.init()
 speaker.setProperty('rate',150)
 
 
 def getInfoFromWikipedia():
-    wiki_wiki = wikipediaapi.Wikipedia('en')
+    wiki_wiki = wikipediaapi.Wikipedia('de')
 
     info = wiki_wiki.page('Python_(programming_language)')
 
-    if info.exists() == True:
+    if info.exists() == False:
         speaker.say("Die Seite existiert nicht oder ich habe es nicht verstanden!")
         speaker.runAndWait()
 
@@ -31,18 +31,45 @@ def getInfoFromWikipedia():
 def getAquariumValues():
     pass
 
+def sayGoodbye():
+    pass
+
+def getWeatherData():
+    pass
+
+def getStocksValues():
+    pass
 
 
+def googleSearch():
+    search_results = google.search("This is my query", 1)
+    for result in search_results:
+        print(result.description)
+        speaker.say(result.description)
+        speaker.runAndWait()
 
-mappings = {'hi' , getAquariumValues(),
+
+mappings = {'aquarium' , getAquariumValues(),
             'wikipedia', getInfoFromWikipedia(),
+            'abschied' , sayGoodbye(),
+            'wetter' ,  getWeatherData(),
+            'aktie', getStocksValues(),
+            'internet', googleSearch()
             }
-
-
-
 
 
 #the json file contains commands for the assistent
 assistent = GenericAssistant('commands.json',intent_methods=mappings)
 assistent.train_model()
-assistent.save_model()
+assistent.train_model()
+
+
+speaker.say("Ich bin bereit")
+speaker.runAndWait()
+
+googleSearch()
+
+while True:
+    pass
+
+
